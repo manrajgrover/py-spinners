8ee0afd0377c04021288de3b85ded67bda13ea13
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-14 22:13:47 +0530

    Setup.py: Initialized

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='py-spinners',
+    packages=['py-spinners'],
+    version='0.0.1',
+    description='Spinners for terminals',
+    author='Manraj Singh',
+    author_email='manrajsinghgrover@gmail.com',
+    url='https://github.com/ManrajGrover/py-spinners',
+    keywords=[
+        'cli',
+        'spinner',
+        'spinners',
+        'terminal',
+        'term',
+        'console',
+        'ascii',
+        'unicode',
+        'loading',
+        'indicator',
+        'progress',
+        'busy',
+        'wait',
+        'idle',
+        'json'
+    ]
+)
