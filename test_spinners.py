45f7d81b7ecf855044689cb989bbd1c8f9d95d9a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-21 22:03:51 +0530

    Tests: Initialized

---
+++ test_spinners.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""This module tests Spinners enum."""
+import re
+import unittest
+
+from enum import Enum
+from spinners import Spinners
+
+
+class TestSpinners(unittest.TestCase):
+    """Test Spinners enum for attribute values."""
+
+    def test_symbols(self):
+        """Test the symbols in Spinners enum."""
+        self.assertTrue(
+            issubclass(Spinners, Enum)
+        )
+
+        dots = Spinners.dots
+
+        self.assertEquals(
+            dots.value['interval'], 80
+        )
+
+        self.assertEquals(
+            type(dots.value['frames']), list
+        )
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestSpinners)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
