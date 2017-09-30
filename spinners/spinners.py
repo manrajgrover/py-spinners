8d8e6b58f3fa3551bd796c391bfe3c810276f2ea
Manraj Singh <manrajsinghgrover@gmail.com>
2017-09-30 16:48:03 +0530

    Fixes #6: Integrate tox and unicode errors in builds (#8)
    * Integrates tox
    
    * Spinners: Defines encoding for file object
    
    * Version: Bumped to 0.0.16

--- spinners/spinners.py
+++ spinners/spinners.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Python wrapper for beautiful terminal spinner library by @sindresorhus.
 
@@ -15,6 +16,8 @@ from __future__ import unicode_literals, absolute_import
 from enum import Enum
 import json
 import os
+import io
+
 from spinners.constants import CLI_SPINNERS_DIR, SPINNER_FILE
 import logging
 
@@ -28,7 +31,7 @@ CURR_DIR = os.path.join(os.path.dirname(__file__), '..')
 """
 Read the spinners from cli-spinner submodule
 """
-with open(os.path.join(CURR_DIR, CLI_SPINNERS_DIR, SPINNER_FILE)) as data:
+with io.open(os.path.join(CURR_DIR, CLI_SPINNERS_DIR, SPINNER_FILE), encoding='utf-8') as data:
     spinners = json.load(data)
 
 Spinners = Enum('Spinners', spinners)
